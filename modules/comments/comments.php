48f748714c6c277e2b8504afe92e8ea5bef41c61
RomanNikolaevich <85992849+RomanNikolaevich@users.noreply.github.com>
2022-05-13 02:04:20 +0300

    почти все работает, но нужно допилить

--- modules/comments/comments.php
+++ modules/comments/comments.php
@@ -1,8 +1,8 @@
 <?php
-if (!empty($_POST['loginComm']) and !empty($_POST['password'])) {
+if (!empty($_POST['loginComm']) and !empty($_POST['textComm'])) {
 	$loginComm = $_POST['loginComm'];
 	$textComm = $_POST['textComm'];
-
+	$link = mysqli_connect("localhost", "root", "root", "agrodb");
 	$query = "INSERT INTO comments SET name='$loginComm', text='$textComm'";
 	mysqli_query($link, $query);
 }
